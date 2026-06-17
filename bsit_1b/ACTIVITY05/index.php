75d908574dc962e25e86043c541d692cf238c373
ravina17dev <s2501927@usls.edu.ph>
2026-06-17 13:10:32 +0800

    Add files via upload

---
+++ bsit_1b/ACTIVITY05/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>ACT 04</title>
+        <link rel="stylesheet" type="text/css" href="css/styless.css" />
+</head>
+<body>
+    <div class="wrapper">
+        <div class="header">
+
+        </div>
+    <div class="container">
+
+        </div>
+    <div class="footer">
+        
+</div>  
+</body>
+</html>
